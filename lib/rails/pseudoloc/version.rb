f9f251d3ec3896d8782f96f57b3a4e12b800a7b7
Lee Dohm <lee@liftedstudios.com>
2013-11-08 18:18:29 -0800

    Update version number

--- lib/rails/pseudoloc/version.rb
+++ lib/rails/pseudoloc/version.rb
@@ -6,6 +6,6 @@ module Rails
   # Provides automated pseudolocalization capabilites to Ruby on Rails applications.
   module Pseudoloc
     # Version number of the gem.
-    VERSION = '0.0.1'
+    VERSION = '0.0.2'
   end
 end
